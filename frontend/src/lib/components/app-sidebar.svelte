1c0a972749b00426156ddf0b5ae189f2a6415e3c
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-03-04 14:23:23 -0800

    feat: add basic (non-functioning) sidebar

---
+++ frontend/src/lib/components/app-sidebar.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import {
+		Sidebar,
+		SidebarContent,
+		SidebarGroupContent,
+		SidebarGroupLabel,
+		SidebarMenu,
+		SidebarMenuButton,
+		SidebarMenuItem,
+	} from './ui/sidebar';
+
+	import RSS from 'lucide-svelte/icons/rss';
+
+	const feeds = [
+		{
+			title: 'lux@web',
+			url: '#',
+		},
+		{
+			title: 'testing',
+			url: '#',
+		},
+	];
+</script>
+
+<Sidebar>
+	<SidebarContent>
+		<SidebarGroupLabel>Feeds</SidebarGroupLabel>
+		<SidebarGroupContent>
+			<SidebarMenu>
+				{#each feeds as feed (feed.title)}
+					<SidebarMenuItem>
+						<SidebarMenuButton>
+							{#snippet child({ props })}
+								<a href={feed.url} {...props}>
+									<RSS />
+									<span>{feed.title}</span>
+								</a>
+							{/snippet}
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				{/each}
+			</SidebarMenu>
+		</SidebarGroupContent>
+	</SidebarContent>
+</Sidebar>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
+	import { SidebarProvider, SidebarTrigger } from '$lib/components/ui/sidebar';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<SidebarProvider>
+	<AppSidebar />
+	<main>
+		<SidebarTrigger />
+		{@render children?.()}
+	</main>
+</SidebarProvider>
